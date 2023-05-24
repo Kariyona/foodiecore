df26943d4d5943eeec739024cc76389fdb4b015c
kariyona <princesswyona@gmail.com>
2023-05-24 14:54:14 -0700

    removed profile

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,7 +14,7 @@ import CreateReviewForm from "./components/Reviews/CreateReviewForm";
 
 import Sidebar from "./components/Navigation/Sidebar";
 import UserListings from "./components/Listings/UserListings";
-import Profile from "./components/Profile";
+// import Profile from "./components/Profile";
 
 function App() {
   const dispatch = useDispatch();
