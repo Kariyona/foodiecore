65d719345d42d33b742099ad5e10a19bf74122ee
kariyona <princesswyona@gmail.com>
2023-07-11 17:32:55 -0700

    new tab

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -20,9 +20,9 @@ const Splash = () => {
               </NavLink>
             </div>
             <div className="about-links">
-            <a href="https://github.com/Kariyona" title="GitHub">
+            <a href="https://github.com/Kariyona" title="GitHub" target="_blank">
               <i className="fa-brands fa-github" title="GitHub"></i></a>
-            <a href="https://www.linkedin.com/in/wyonadev/" title="LinkedIn">
+            <a href="https://www.linkedin.com/in/wyonadev/" title="LinkedIn" target="_blank">
               <i className="fa-brands fa-linkedin"></i></a>
             </div>
           </div>
